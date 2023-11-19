139b97f0b9c338c0f36e4325a2d3cf2dbb49080a
Farnaz <farnaz@happycoding.it>
2023-11-19 17:31:40 +0100

    First Commit
    First Date:19.11.2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      newUser:[],
+    }
+  }
+  componentDidMount() { 
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response =>response.json())
+    .then(data => this.setState({newUser: data}))
+    
+   }
+  render() { 
+    
+    return ( 
+      <div>
+        {this.state.newUser.length >0 ? 
+        (<Container>
+          <Table striped bordered hover size="sm" className="mt-3">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Username</th>
+                  <th> Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              {this.state.newUser.map((user)=>{
+                return (
+                 <tbody>
+                 <tr>
+                   <td>{user.id}</td>
+                   <td>{user.name}</td>
+                   <td>{user.username}</td>
+                   <td>{user.email}</td>
+                 </tr>
+ 
+               </tbody>
+               )
+
+              })}
+             
+          </Table>
+        </Container>):
+        (  <Alert variant="success">
+        <Alert.Heading>Loading!</Alert.Heading>
+      
+      </Alert>)
+        }
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
